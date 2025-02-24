92e754ac81c1da4f6f22a955e012f806294e6df6
imetandy <robinson.andrewjames@gmail.com>
2025-02-24 09:51:08 +0100

    folder cleanup

--- scripts/build_wheels.sh
+++ scripts/build_wheels.sh
@@ -10,13 +10,13 @@ docker buildx build \
   --platform linux/amd64 \
   --target wheels \
   -o type=local,dest=.wheels/amd64 \
-  -f worker/builder/Dockerfile .
+  -f builder/Dockerfile .
 
 echo -e "\033[1;33mBuilding wheels for linux/arm64 (aarch64)...\033[0m"
 docker buildx build \
   --platform linux/arm64 \
   --target wheels \
   -o type=local,dest=.wheels/aarch64 \
-  -f worker/builder/Dockerfile .
+  -f builder/Dockerfile .
 
 echo "Wheels built successfully for both linux/amd64 and linux/arm64." 
